57de2934675b7d75209b66a49ba6b89c7d0458da
Joseph Hart <josephrhart@gmail.com>
2022-01-21 13:12:45 -0600

    adding layout override example

--- src/pages/hello/[name].tsx
+++ src/pages/hello/[name].tsx
@@ -1,36 +1,16 @@
-import type { GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
 
-interface HelloProps {
-  // name: string;
-  catFact: string;
-}
-
-const Hello = ({
-  // name,
-  catFact,
-}: HelloProps): JSX.Element => {
+const Hello = (): JSX.Element => {
   const { name } = useRouter().query;
   return (
     <div>
       <h1>Hello, {name}!</h1>
-      <h3>{catFact}</h3>
     </div>
   );
 };
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  // can also get routing values here
-  const { name } = context.query;
-
-  const { fact } = await fetch('https://catfact.ninja/fact').then((res) => res.json());
-
-  return {
-    props: {
-      name,
-      catFact: fact,
-    },
-  };
-}
+// override default site layout
+Hello.getLayout = (page: ReactNode) => page;
 
 export default Hello;
